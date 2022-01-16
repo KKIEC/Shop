2332e81545e67567e42c336560b61e24d4538a0a
Krzysztof Kiec <kkiec95@gmail.com>
2022-01-16 20:02:55 +0100

    Success and cancel options for checkout

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :products
   post 'checkout/create', to: 'checkout#create'
   resources :webhooks, only: %i[create]
+  get 'success', to: 'checkout#success'
+  get 'failure', to: 'checkout#cancel'
 end
